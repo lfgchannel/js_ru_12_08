4c5ce1d8621804fe54537470f0bbdbc15aa84b80
Roman <kaboom.ua@gmail.com>
2016-09-05 23:54:42 +0300

    Update commentIdGenerator.js

--- src/middlewares/commentIdGenerator.js
+++ src/middlewares/commentIdGenerator.js
@@ -4,8 +4,10 @@ export default store => next => action => {
 
     const { payload, type } = action
 
+    //Мидлвары должны быть максимально универсальными. Тебе и в будущем может понадобиться логика генерации id, не привязывайся к типу екшина
     switch(type) {
         case ADD_COMMENT:
+            //очень плохая логика для генерации id, где-то удалишь и не будет никакой уникальности - лучше просто random или guid
             const id = store.getState().comments.size
             next({type, payload: {...payload, id}})
             break
